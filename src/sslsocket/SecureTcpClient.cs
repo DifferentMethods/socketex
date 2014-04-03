1f5c6e2e31aafba89162529af2e9728845cfea72
Simon Wittber <simonwittber@gmail.com>
2014-04-03 16:45:06 +0800

    Simplified TcpClient API.

--- src/sslsocket/SecureTcpClient.cs
+++ src/sslsocket/SecureTcpClient.cs
@@ -1,43 +1,41 @@
-﻿using System.IO;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using Org.BouncyCastle.Crypto.Tls;
 
 namespace SocketEx
 {
-    public class SecureTcpClient : TcpClient
-    {
-        private readonly TlsClient tlsClient;
-        private Stream secureStream;
+	public class SecureTcpClient : TcpClient
+	{
+		private readonly TlsClient tlsClient;
+		private Stream secureStream;
 
-        public SecureTcpClient(string host, int port)
-            : this(host, port, new LegacyTlsClient(new AlwaysValidVerifyer()))
-        {
+		public SecureTcpClient (string host, int port)
+            : this (host, port, new LegacyTlsClient (new AlwaysValidVerifyer ()))
+		{
 
-        }
+		}
 
-        public SecureTcpClient(string host, int port, TlsClient tlsClient)
-            : base(AddressFamily.InterNetwork)
-        {
-            this.tlsClient = tlsClient;
+		public SecureTcpClient (string host, int port, TlsClient tlsClient)
+            : base (AddressFamily.InterNetwork)
+		{
+			this.tlsClient = tlsClient;
+			this.host = host;
+			this.port = port;
+			InnerConnect ();
+		}
 
-            var myEndpoint = new DnsEndPoint(host, port);
-            InnerConnect(myEndpoint);
-        }
+		public override Stream GetStream ()
+		{
+			return secureStream == null ? base.GetStream () : this.secureStream;
+		}
 
-        public override Stream GetStream()
-        {
-            return secureStream == null ? base.GetStream() : this.secureStream;
-        }
-
-        protected override void HandleConnectionReady()
-        {
-            var stream = this.GetStream();
-
-            var handler = new TlsProtocolHandler(stream);
-            handler.Connect(tlsClient);
-
-            this.secureStream = handler.Stream;
-        }
-    }
+		protected override void HandleConnectionReady ()
+		{
+			var stream = this.GetStream ();
+			var handler = new TlsProtocolHandler (stream);
+			handler.Connect (tlsClient);
+			this.secureStream = handler.Stream;
+		}
+	}
 }
--- src/tcpclient/TcpClient.cs
+++ src/tcpclient/TcpClient.cs
@@ -1,4 +1,3 @@
-﻿// The code is based on the Crystalbyte Networking for Silverlight. http://cbnet.codeplex.com/
 // Modified to work with Windows Phone by Mikael Koskinen. http://mikaelkoskinen.net
 
 using System;
@@ -10,218 +9,98 @@ using System.Threading;
 
 namespace SocketEx
 {
-    /// <summary>
-    /// The TcpClient class provides simple methods for connecting, sending, and receiving stream data over a network in synchronous blocking mode.
-    /// </summary>
-    public class TcpClient : IDisposable
-    {
-        private readonly AutoResetEvent autoResetEvent;
-        private readonly NetworkStream networkStream;
-        private EndPoint endpoint;
-        private bool responsePending;
+	/// <summary>
+	/// The TcpClient class provides simple methods for connecting, sending, and receiving stream data over a network in synchronous blocking mode.
+	/// </summary>
+	public class TcpClient : IDisposable
+	{
+		private readonly AutoResetEvent autoResetEvent;
+		private readonly NetworkStream networkStream;
+		private bool responsePending;
+		protected string host;
+		protected int port;
 
-        public TcpClient()
-            : this(AddressFamily.InterNetwork)
-        {
-        }
+		public Socket Client { get; set; }
 
+		public TcpClient ()
+			: this (AddressFamily.InterNetwork)
+		{
+		}
 
-        public TcpClient(IPEndPoint endpoint)
-            : this(AddressFamily.InterNetwork)
-        {
-            Connect(endpoint);
-        }
+		public TcpClient (string host, int port)
+			: this (AddressFamily.InterNetwork)
+		{
+			this.host = host;
+			this.port = port;
+			InnerConnect ();
+		}
 
-        public TcpClient(string host, int port)
-            : this(AddressFamily.InterNetwork)
-        {
-            var myEndpoint = new DnsEndPoint(host, port);
-            InnerConnect(myEndpoint);
-        }
+		public TcpClient (AddressFamily addressFamily)
+		{
+			Client = new Socket (addressFamily, SocketType.Stream, ProtocolType.Tcp);
+			networkStream = new NetworkStream (Client);
+		}
 
-        public TcpClient(AddressFamily addressFamily)
-        {
-            autoResetEvent = new AutoResetEvent(false);
+		public int Available {
+			get { throw new NotSupportedException (); }
+		}
 
-            Client = new Socket(addressFamily, SocketType.Stream, ProtocolType.Tcp);
-            networkStream = new NetworkStream(Client);
-        }
+		public bool Connected {
+			get { return Client != null && Client.Connected; }
+		}
 
-        public int Available
-        {
-            get { throw new NotSupportedException(); }
-        }
+		public bool Active {
+			get { return Connected; }
+		}
 
-        public Socket Client { get; set; }
+		public bool ExclusiveAddressUse {
+			get { return false; }
+		}
 
-        public bool Connected
-        {
-            get { return Client != null && Client.Connected; }
-        }
+		public bool NoDelay {
+			get { return true; }
+			set { throw new NotImplementedException (); }
+		}
 
-        public bool Active
-        {
-            get { return Connected; }
-        }
+		#region IDisposable Members
 
-        public bool ExclusiveAddressUse
-        {
-            get { return false; }
-        }
+		public void Dispose ()
+		{
+			var stream = GetStream ();
+			stream.Dispose ();
 
-        public bool NoDelay
-        {
-            get { return true; }
-            set { throw new NotImplementedException(); }
-        }
+			try {
+				Client.Shutdown (SocketShutdown.Both);
+				Client.Close ();
+			} catch (ObjectDisposedException ex) {
+				Debug.WriteLine (ex.Message);
+			} catch (SocketException ex) {
+				Debug.WriteLine (ex.Message);
+			}
+		}
 
-        #region IDisposable Members
+		#endregion
 
-        public void Dispose()
-        {
-            var stream = GetStream();
-            stream.Dispose();
+		public void Connect (string host, int port)
+		{
+			this.host = host;
+			this.port = port;
+			InnerConnect ();
+		}
 
-            try
-            {
-                Client.Shutdown(SocketShutdown.Both);
-                Client.Close();
-            }
-            catch (ObjectDisposedException ex)
-            {
-                Debug.WriteLine(ex.Message);
-            }
-            catch (SocketException ex)
-            {
-                Debug.WriteLine(ex.Message);
-            }
-        }
+		protected virtual void HandleConnectionReady ()
+		{
+		}
 
-        #endregion
+		protected void InnerConnect ()
+		{
+			Client.Connect (this.host, this.port);
+			HandleConnectionReady ();
+		}
 
-        public IAsyncResult BeginConnect(IPAddress address, int port, AsyncCallback requestCallback, object userToken)
-        {
-            endpoint = new IPEndPoint(address, port);
-            return BeginConnect(requestCallback, userToken);
-        }
-
-        public IAsyncResult BeginConnect(IPAddress[] addresses, int port, AsyncCallback requestCallback,
-                                         object userToken)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IAsyncResult BeginConnect(string host, int port, AsyncCallback requestCallback, object userToken)
-        {
-            endpoint = new DnsEndPoint(host, port);
-            return BeginConnect(requestCallback, userToken);
-        }
-
-        private IAsyncResult BeginConnect(AsyncCallback requestCallback, object userToken)
-        {
-            var stateObject = new StateObject
-                                  {
-                                      AsyncState = userToken,
-                                      Callback = requestCallback,
-                                      IsCompleted = false
-                                  };
-
-            ConnectAsync(stateObject);
-            return stateObject;
-        }
-
-        private void OnConnectedAsync(object sender, SocketAsyncEventArgs e)
-        {
-            Continue();
-
-            var stateObject = e.UserToken as StateObject;
-            if (stateObject == null) return;
-
-            stateObject.IsCompleted = true;
-            stateObject.Callback(stateObject);
-        }
-
-        private void OnConnected(object sender, SocketAsyncEventArgs e)
-        {
-            Continue();
-        }
-
-        private void ConnectAsync(StateObject stateObject)
-        {
-            var e = new SocketAsyncEventArgs { UserToken = stateObject, RemoteEndPoint = endpoint };
-            e.Completed += OnConnectedAsync;
-            Client.ConnectAsync(e);
-        }
-
-        public void Connect(IPEndPoint myEndpoint)
-        {
-            InnerConnect(myEndpoint);
-        }
-
-        public void Connect(IPAddress address, int port)
-        {
-            var myEndpoint = new IPEndPoint(address, port);
-            InnerConnect(myEndpoint);
-        }
-
-        public void Connect(IPAddress[] address, int port)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Connect(string host, int port)
-        {
-            var myEndPoint = new DnsEndPoint(host, port);
-            InnerConnect(myEndPoint);
-        }
-
-        protected void InnerConnect(EndPoint myEndpoint)
-        {
-            this.endpoint = myEndpoint;
-
-            var e = new SocketAsyncEventArgs { RemoteEndPoint = this.endpoint };
-            e.Completed += OnConnected;
-
-            try
-            {
-                Client.ConnectAsync(e);
-                WaitOne();
-
-                HandleConnectionReady();
-            }
-            catch (SocketException)
-            {
-                Continue();
-            }
-        }
-
-        protected virtual void HandleConnectionReady()
-        {
-                
-        }
-
-        public void EndConnect(IAsyncResult asyncResult)
-        {
-            if (!responsePending)
-            {
-                WaitOne();
-            }
-        }
-
-        private void Continue()
-        {
-            responsePending = false;
-            autoResetEvent.Set();
-        }
-
-        private void WaitOne()
-        {
-            autoResetEvent.WaitOne();
-        }
-
-        public virtual Stream GetStream()
-        {
-            return networkStream;
-        }
-    }
+		public virtual Stream GetStream ()
+		{
+			return networkStream;
+		}
+	}
 }
