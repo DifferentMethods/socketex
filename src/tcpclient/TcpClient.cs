1d9b57b295659da84ea8a2f4b3a5e477f0ba2530
Simon Wittber <simonwittber@gmail.com>
2014-04-04 16:47:42 +0800

    restored TcpClient to async implementation.

--- src/tcpclient/TcpClient.cs
+++ src/tcpclient/TcpClient.cs
@@ -1,3 +1,4 @@
+// The code is based on the Crystalbyte Networking for Silverlight. http://cbnet.codeplex.com/
 // Modified to work with Windows Phone by Mikael Koskinen. http://mikaelkoskinen.net
 
 using System;
@@ -16,27 +17,31 @@ namespace SocketEx
 	{
 		private readonly AutoResetEvent autoResetEvent;
 		private readonly NetworkStream networkStream;
+		private EndPoint endpoint;
 		private bool responsePending;
-		protected string host;
-		protected int port;
-
-		public Socket Client { get; set; }
 
 		public TcpClient ()
 			: this (AddressFamily.InterNetwork)
 		{
 		}
 
+		public TcpClient (IPEndPoint endpoint)
+			: this (AddressFamily.InterNetwork)
+		{
+			Connect (endpoint);
+		}
+
 		public TcpClient (string host, int port)
 			: this (AddressFamily.InterNetwork)
 		{
-			this.host = host;
-			this.port = port;
-			InnerConnect ();
+			var myEndpoint = new DnsEndPoint (host, port);
+			InnerConnect (myEndpoint);
 		}
 
 		public TcpClient (AddressFamily addressFamily)
 		{
+			autoResetEvent = new AutoResetEvent (false);
+
 			Client = new Socket (addressFamily, SocketType.Stream, ProtocolType.Tcp);
 			networkStream = new NetworkStream (Client);
 		}
@@ -44,6 +49,8 @@ namespace SocketEx
 		public int Available {
 			get { throw new NotSupportedException (); }
 		}
+
+		public Socket Client { get; set; }
 
 		public bool Connected {
 			get { return Client != null && Client.Connected; }
@@ -81,21 +88,120 @@ namespace SocketEx
 
 		#endregion
 
+		public IAsyncResult BeginConnect (IPAddress address, int port, AsyncCallback requestCallback, object userToken)
+		{
+			endpoint = new IPEndPoint (address, port);
+			return BeginConnect (requestCallback, userToken);
+		}
+
+		public IAsyncResult BeginConnect (IPAddress[] addresses, int port, AsyncCallback requestCallback,
+		                                 object userToken)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public IAsyncResult BeginConnect (string host, int port, AsyncCallback requestCallback, object userToken)
+		{
+			endpoint = new DnsEndPoint (host, port);
+			return BeginConnect (requestCallback, userToken);
+		}
+
+		private IAsyncResult BeginConnect (AsyncCallback requestCallback, object userToken)
+		{
+			var stateObject = new StateObject {
+				AsyncState = userToken,
+				Callback = requestCallback,
+				IsCompleted = false
+			};
+
+			ConnectAsync (stateObject);
+			return stateObject;
+		}
+
+		private void OnConnectedAsync (object sender, SocketAsyncEventArgs e)
+		{
+			Continue ();
+
+			var stateObject = e.UserToken as StateObject;
+			if (stateObject == null)
+				return;
+
+			stateObject.IsCompleted = true;
+			stateObject.Callback (stateObject);
+		}
+
+		private void OnConnected (object sender, SocketAsyncEventArgs e)
+		{
+			Continue ();
+		}
+
+		private void ConnectAsync (StateObject stateObject)
+		{
+			var e = new SocketAsyncEventArgs { UserToken = stateObject, RemoteEndPoint = endpoint };
+			e.Completed += OnConnectedAsync;
+			Client.ConnectAsync (e);
+		}
+
+		public void Connect (IPEndPoint myEndpoint)
+		{
+			InnerConnect (myEndpoint);
+		}
+
+		public void Connect (IPAddress address, int port)
+		{
+			var myEndpoint = new IPEndPoint (address, port);
+			InnerConnect (myEndpoint);
+		}
+
+		public void Connect (IPAddress[] address, int port)
+		{
+			throw new NotImplementedException ();
+		}
+
 		public void Connect (string host, int port)
 		{
-			this.host = host;
-			this.port = port;
-			InnerConnect ();
+			var myEndPoint = new DnsEndPoint (host, port);
+			InnerConnect (myEndPoint);
+		}
+
+		protected void InnerConnect (EndPoint myEndpoint)
+		{
+			this.endpoint = myEndpoint;
+
+			var e = new SocketAsyncEventArgs { RemoteEndPoint = this.endpoint };
+			e.Completed += OnConnected;
+
+			try {
+				Client.ConnectAsync (e);
+				WaitOne ();
+
+				HandleConnectionReady ();
+			} catch (SocketException) {
+				Continue ();
+			}
 		}
 
 		protected virtual void HandleConnectionReady ()
 		{
+
 		}
 
-		protected void InnerConnect ()
+		public void EndConnect (IAsyncResult asyncResult)
 		{
-			Client.Connect (this.host, this.port);
-			HandleConnectionReady ();
+			if (!responsePending) {
+				WaitOne ();
+			}
+		}
+
+		private void Continue ()
+		{
+			responsePending = false;
+			autoResetEvent.Set ();
+		}
+
+		private void WaitOne ()
+		{
+			autoResetEvent.WaitOne ();
 		}
 
 		public virtual Stream GetStream ()
